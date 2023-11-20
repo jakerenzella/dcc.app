7487ac07cf001fdf3ed501f90ebfbb642f17be95
jakerenzella <j@kerenzella.com>
2023-11-20 21:59:40 +1100

    fix: add npm to package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,14 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
+  site: "https://dcc.cse.unsw.edu.au",
   integrations: [tailwind(), mdx(), sitemap(), react()],
+  output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
 });
