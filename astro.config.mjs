c92ddca253b4d86ea9e8b4ee5f39c9c9cdf16b70
jakerenzella <j@kerenzella.com>
2023-11-21 15:07:26 +1100

    new: add google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,24 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
-
 import node from "@astrojs/node";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://dcc.cse.unsw.edu.au",
-  integrations: [tailwind(), mdx(), sitemap(), react()],
+  integrations: [
+    tailwind(),
+    mdx(),
+    sitemap(),
+    react(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
   output: "server",
   adapter: node({
     mode: "standalone",
