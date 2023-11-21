36e2cec98d0180508d16b7bbeac6d3307417c03e
jakerenzella <j@kerenzella.com>
2023-11-21 15:32:09 +1100

    fix: remove partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,24 +3,13 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
-import node from "@astrojs/node";
 
-import partytown from "@astrojs/partytown";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://dcc.cse.unsw.edu.au",
-  integrations: [
-    tailwind(),
-    mdx(),
-    sitemap(),
-    react(),
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
-  ],
+  integrations: [tailwind(), mdx(), sitemap(), react()],
   output: "server",
   adapter: node({
     mode: "standalone",
